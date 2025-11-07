fb79572308f86ac60e538cdbb47b67ff8366253d
Lovelej <love.gruvberg@gmail.com>
2025-11-07 11:54:38 +0100

    Added Effect class ( Effect.cpp, Effect.h ). We should do a code review on this D:

--- PlayerEffects/Private/Effect.cpp
+++ PlayerEffects/Private/Effect.cpp
@@ -1,2 +1,15 @@
 #include "../Public/Effect.h"
 
+Effect::Effect(const std::string& name) {}
+
+void Effect::apply() {
+
+}
+
+void Effect::remove() {
+	
+}
+
+const std::string Effect::getName() const { 	
+	return name; 
+}
