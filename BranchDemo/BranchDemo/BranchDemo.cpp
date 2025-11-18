6c4c552e79839b4c4b27d4687aeeb685c6f854fc
Carl Johan Freme <cj.freme@gmail.com>
2025-11-18 09:26:57 +0100

    Simple program starting point for demoing branching.

---
+++ BranchDemo/BranchDemo/BranchDemo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;  // Basfunktion att ändra i demonstration
+}
+
+int main() {
+    std::cout << "Add 2 + 3 = " << add(2, 3) << "\n";
+    return 0;
+}
